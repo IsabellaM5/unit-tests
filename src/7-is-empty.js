88d145e2eb395befeeb151e4a1f51a42c8e962da
Your Name <isabellamarkskog@live.se>
2021-02-28 23:19:20 +0100

    Solved test 8

--- src/7-is-empty.js
+++ src/7-is-empty.js
@@ -4,7 +4,7 @@ export const isEmpty = (stringArrayOrObject) => {
   if (type === 'string') {
     return stringArrayOrObject === ''
   } else if (type === 'object') {
-    return Object.keys(stringArrayOrObject).length === 0
+    return Object.keys(stringArrayOrObject).length === 0 //https://www.samanthaming.com/tidbits/94-how-to-check-if-object-is-empty/
   } 
 
 }
