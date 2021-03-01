7e3c1d34597b313099e77b53352629cee564461a
Your Name <isabellamarkskog@live.se>
2021-03-01 08:47:05 +0100

    Added comment on test 9

--- src/9-hashtags.js
+++ src/9-hashtags.js
@@ -1,3 +1,3 @@
 export const hashtags = (text) => {
-  return text.match(/#([\S]+)/igm);
+  return text.match(/#([\S]+)/igm);  //https://stackoverflow.com/questions/432493/how-do-you-access-the-matched-groups-in-a-javascript-regular-expression
 }
