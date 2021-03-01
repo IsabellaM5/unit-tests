5ade96e72a46f482bb3d53e78c6ca1306b488317
Your Name <isabellamarkskog@live.se>
2021-03-01 13:47:19 +0100

    Updated the code in the different tests generally.

--- src/4-angle-calculator.js
+++ src/4-angle-calculator.js
@@ -1,3 +1,3 @@
 export const angleCalculator = (turns) => {
-  return 360 * turns
+  return turns * 360
 }
--- src/9-hashtags.js
+++ src/9-hashtags.js
@@ -1,3 +1,3 @@
 export const hashtags = (text) => {
-  return text.match(/#([\S]+)/igm);  //https://stackoverflow.com/questions/432493/how-do-you-access-the-matched-groups-in-a-javascript-regular-expression
-}
+  return text.match(/#([\S]+)/igm);  //https://stackoverflow.com/questions/432493/how-do-you-access-the-matched-groups-in-a-javascript-regular-expression 
+}                                    // I found the answer by looking in the above thread on StackOverflow. /IM
